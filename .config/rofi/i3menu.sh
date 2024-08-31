99aa4c66f8b0e3836ac604672dda0c79bdd28df3
Chase Nickel <nickel5@outlook.com>
2024-08-31 01:02:11 -0500

    Better rofi power menu. Rofi i3 menu. Paper icons

---
+++ .config/rofi/i3menu.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+if [ -z "$@" ]; then
+    echo -en "Restart\0icon\x1fsystem-restart\n"
+    echo -en "Reload Config\0icon\x1fsystem-log-out\n"
+else
+    if [ "$1" = "Restart" ]; then
+        i3-msg -q restart
+    elif [ "$1" = "Reload Config" ]; then
+        i3-msg -q reload
+    fi
+fi
