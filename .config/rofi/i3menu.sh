814c36ac7c6de7b9a0a19ba979fe859f49de4814
Chase Nickel <nickel5@outlook.com>
2024-09-09 01:29:43 -0500

    I love kakoune so much

--- .config/rofi/i3menu.sh
+++ .config/rofi/i3menu.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 if [ -z "$@" ]; then
-    # echo -en "Restart\0icon\x1freplay\n"
-    # echo -en "Reload Config\0icon\x1fsync\n"
-    echo -en "Restart\0icon\x1fsystem-restart\n"
-    echo -en "Reload Config\0icon\x1fsystem-log-out\n"
+    echo -en "Restart\0icon\x1freplay\n"
+    echo -en "Reload Config\0icon\x1fsync\n"
+    # echo -en "Restart\0icon\x1fsystem-restart\n"
+    # echo -en "Reload Config\0icon\x1fsystem-log-out\n"
 else
     if [ "$1" = "Restart" ]; then
         i3-msg -q restart
